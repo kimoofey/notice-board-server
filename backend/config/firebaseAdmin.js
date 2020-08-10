b31995815c138ae8c3fcf3502a76888e2448a1f4
Andrey Kim <kimoofey@gmail.com>
2020-08-10 22:19:42 +0300

    fix: reconfigure receiving secrets

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -2,7 +2,16 @@ const admin = require('firebase-admin');
 let serviceAccount = {};
 
 if (process.env.NODE_ENV === 'production') {
-    serviceAccount = process.env.FIREBASEADMINKEY;
+    serviceAccount.type = process.env.TYPE;
+    serviceAccount.project_id = process.env.PROJECTID;
+    serviceAccount.private_key_id = process.env.PRIVATE_KEY_ID;
+    serviceAccount.private_key = process.env.PRIVATE_KEY;
+    serviceAccount.client_email = process.env.CLIENT_EMAIL;
+    serviceAccount.client_id = process.env.CLIENT_ID;
+    serviceAccount.auth_uri = process.env.AUTH_URI;
+    serviceAccount.token_uri = process.env.TOKEN_URI;
+    serviceAccount.auth_provider_x509_cert_url = process.env.AUTH_PROVIDER_X509_CERT_URL;
+    serviceAccount.client_x509_cert_url = process.env.CLIENT_X509_CERT_URL;
 } else {
     serviceAccount = require('./adminAccountKey.json');
 }
