ad62c6aa1d56f8505c94c8926ad63d96063529c0
Andrey Kim <kimoofey@gmail.com>
2020-08-18 22:00:37 +0300

    feat: add post request for messages

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const firebase = require('../../config/firebase');
 const admin = require('../../config/firebaseAdmin');
 const db = require('../../config/firebaseDB');
 const messagesCollection = 'Messages';
@@ -21,7 +20,28 @@ router.get('/', async (req, res) => {
                 },
             );
     } catch (error) {
-        console.log(error);
+        return res.status(500).send(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const itemMessage = {
+            _id: req.body._id,
+            text: req.body.text,
+            createdAt: req.body.createdAt,
+            idFrom: req.body.currentUserId,
+            idTo: req.body.id,
+            user: JSON.parse(req.body.user),
+        };
+        const result = await admin.firestore()
+            .collection('Messages')
+            .doc(req.body.groupChatId)
+            .collection(req.body.groupChatId)
+            .doc(req.body.timestamp)
+            .set(itemMessage);
+        return res.status(200).json(result);
+    } catch (error) {
         return res.status(500).send(error);
     }
 });
