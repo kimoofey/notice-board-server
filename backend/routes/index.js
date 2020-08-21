cc7b8ee6e75eb94093f62b220bac70f3dd91fff0
Andrey Kim <kimoofey@gmail.com>
2020-08-21 12:52:31 +0300

    feat: add favicon support

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/favicon.ico', (req, res) => {
+    res.sendFile(__dirname + "../public/images/favicon.ico");
+});
+
 module.exports = router;
